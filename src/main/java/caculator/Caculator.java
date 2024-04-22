5d258c317e8a328b662f8ea8c670bc3010b58113
Vuthibichngoc212 <96900628+Vuthibichngoc212@users.noreply.github.com>
2024-04-22 22:42:18 +0700

    first commit

---
+++ src/main/java/caculator/Caculator.java
@@ -0,0 +1,11 @@
+package caculator;
+
+public class Caculator {
+	public int add(int a, int b) {
+        return a + b;
+    }
+ 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
